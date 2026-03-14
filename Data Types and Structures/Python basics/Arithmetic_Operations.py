615e7cdad72aafe7b1c5097821f40dde45af9ef1
Hydra-Dev110 <kiranyadav1102003@gmail.com>
2026-03-14 12:35:57 +0530

    Added Python assignmens

---
+++ Data Types and Structures/Python basics/Arithmetic_Operations.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+#Arithmetic Operators
+print(a + b)
+print(a * b)
+
+#Comparison Operators
+print(a > b)
+print(a == b)
+
+#Logical Operators
+print(a > 5 and b < 5)
+
+#Assignment Operators
+a += 2
+print(a)
+
+#Membership Operators
+print(3 in [1, 2, 3])
